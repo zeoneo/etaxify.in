a18539ff314e3a288ba972fbf96953435b83636c
zeo <zeo29021992@gmail.com>
2021-04-30 14:13:55 +0530

    temp3

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,20 +11,7 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp">
-				<img src="svelte-welcome.png" alt="Welcome"/>
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
+	<h1>Hello World </h1>
 
 	<Counter />
 </section>
